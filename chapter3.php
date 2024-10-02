a86f3f69db7516c43d8fe5f2d42acb472262efb1
荒川えりか <arakawa@example.com>
2024-10-02 04:49:39 +0000

    chapter3演習問題

---
+++ chapter3.php
@@ -0,0 +1,19 @@
+<?php
+
+$price = 1800;
+
+$price_with_tax = $price * 1.10 ;
+
+echo "{$price_with_tax} \n";
+
+$family_name = "arakawa" ;
+
+$first_name = "erika" ;
+
+$name = "$family_name . $first_name . \n";
+
+echo $name;
+
+$name2 = "{$family_name}{$first_name}\n";
+
+echo $name2;
